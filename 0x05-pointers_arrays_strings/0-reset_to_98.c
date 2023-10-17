7c8eec2706ed3cb093e54c296feedbea54120019
Jihane08 <jihaneamini17@gmail.com>
2023-10-17 11:58:46 -0700

    0-reset_to_98.c

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,23 +1,11 @@
-#ifndef MAIN_H
-#define MAIN_H
-#include <stdio.h>
-
+#include "main.h"
 /**
-* void prototypes()
-*char prototypes()
-* int prototypes()
+*reset_to_98 - reset value of an integer to 98
+*@n: parameter targeted
+*Return : nothing
 */
-void reset_to_98(int *n);
-void _puts(char *str);
-void print_rev(char *s);
-void rev_string(char *s);
-void swap_int(int *a, int *b);
-void swap_char(char *a, char *b);
-void puts2(char *str);
-void puts_half(char *str);
-void print_array(int *a, int n);
-char *_strcpy(char *dest, char *src);
-int _putchar(char c);
-int _strlen(char *s);
-int _atoi(char *s);
-#endif /*MAIN_H*/
+
+void reset_to_98(int *n)
+{
+*n = 98;
+}
